ca9b6e2673a379b3a0a276de71d525dc0aa7e2c8
fayssalzakaria <80478344+fayssalzakaria@users.noreply.github.com>
2021-03-15 21:04:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello")
+import cv2
+
+#import de l'algorythme
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# lecture de l'image
+img = cv2.imread('download.jpg')
