1125104b18e3b3383ed63655eb5894cddd488620
fayssalzakaria <80478344+fayssalzakaria@users.noreply.github.com>
2021-03-18 12:08:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import cv2
 
-# Load the cascade
+#import de l'algorythme
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Read the input image
-img = cv2.imread('download.jpg')
+# lecture de l'image
+img = cv2.imread('images.jpg')
 
-# Convert into grayscale
+# convertion en niveau de gris
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces
+# detection du visage
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+#dessin du rectangle autour du visage
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+# afficher la cortie
+cv2.imshow('img', img)
+cv2.waitKey()
